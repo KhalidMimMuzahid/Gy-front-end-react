f425c1ecafc44d851ec6da95cf054fc6b134a7c4
Sarwar3w <sarwar@triplewsols.org>
2023-12-04 15:20:54 +0600

    feat: color game ui created

--- src/containers/UserPages/Games/ColorGames/index.js
+++ src/containers/UserPages/Games/ColorGames/index.js
@@ -1,10 +1,21 @@
 import * as React from "react";
+// for tabs
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
+// for icons
 import { GiLaurelsTrophy } from "react-icons/gi";
+import { IoMdTrophy } from "react-icons/io";
+// for tables
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 const ColorGame = () => {
   // For Tabs
   const [value, setValue] = React.useState("1");
@@ -12,13 +23,30 @@ const ColorGame = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  // for tables
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24),
+    createData("Ice cream sandwich", 237, 9.0, 37),
+    createData("Eclair", 262, 16.0, 24),
+    createData("Cupcake", 305, 3.7, 67),
+    createData("Gingerbread", 356, 16.0, 49),
+  ];
+
   return (
     <div className='color_games_container'>
       {/* For Tabs */}
       <Box sx={{ width: "100%", typography: "body1" }}>
         <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList onChange={handleChange} aria-label='lab API tabs example'>
+            <TabList
+              onChange={handleChange}
+              aria-label='lab API tabs example'
+              centered
+            >
               <Tab label='Parity' value='1' />
               {/* <Tab label='Item Two' value='2' />
               <Tab label='Item Three' value='3' /> */}
@@ -92,7 +120,40 @@ const ColorGame = () => {
             </div>
             <div className='records_table'>
               <div className='table_header'>
-                <p>Table Content goes here</p>
+                <IoMdTrophy size={50} />
+                <p>Parity Record</p>
+              </div>
+              <div className='table-content'>
+                <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell><p>Period</p></TableCell>
+                        <TableCell align='right'><p>Price</p></TableCell>
+                        <TableCell align='right'><p>Number</p></TableCell>
+                        <TableCell align='right'><p>Result</p></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row) => (
+                        <TableRow
+                          key={row.name}
+                          sx={{
+                            "&:last-child td, &:last-child th": { border: 0 },
+                          }}
+                        >
+                          <TableCell component='th' scope='row'>
+                            {row.name}
+                          </TableCell>
+                          <TableCell align='right'>{row.calories}</TableCell>
+                          <TableCell align='right'>{row.fat}</TableCell>
+                          <TableCell align='right'>{row.carbs}</TableCell>
+                          <TableCell align='right'>{row.protein}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               </div>
             </div>
           </TabPanel>
---
+++ src/utils/tools/theme.js
@@ -0,0 +1,11 @@
+// theme.js
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark', // Enable dark mode
+  },
+});
+
+export default theme;
