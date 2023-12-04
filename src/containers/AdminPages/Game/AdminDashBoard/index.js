423e1d1cf345fe5ed9b32b9128471c46580eb291
Sarwar3w <sarwar@triplewsols.org>
2023-12-04 17:43:30 +0600

    feat: admin pannel initial ui done for admin ame dashboard

--- src/containers/AdminPages/Game/AdminDashBoard/index.js
+++ src/containers/AdminPages/Game/AdminDashBoard/index.js
@@ -1,7 +1,83 @@
-import React from "react";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
 
 const AdminGameDashBoard = () => {
-  return <div>AdminGameDashBoard</div>;
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
+
+  return (
+    <div className='game_dashboard_wrapper'>
+      <div className='game_dashboard_header'>
+        <div className="game_dashboard_header_left">
+          <h4>Count Dwon</h4>
+          <h5>0:00</h5>
+        </div>
+        <div className="game_dashboard_header_right">
+          <h4>Active Period Id</h4>
+          <p>0000000000</p>
+        </div>
+      </div>
+      <div className='game_dashboard_table'>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <p>Result</p>
+                </TableCell>
+                <TableCell align='right'>
+                  <p>Numbers</p>
+                </TableCell>
+                <TableCell align='right'>
+                  <p>No. of Users</p>
+                </TableCell>
+                <TableCell align='right'>
+                  <p>Amount</p>
+                </TableCell>
+                <TableCell align='right'>
+                  <p>Action</p>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component='th' scope='row'>
+                    {row.name}
+                  </TableCell>
+                  <TableCell align='right'>{row.calories}</TableCell>
+                  <TableCell align='right'>{row.fat}</TableCell>
+                  <TableCell align='right'>{row.carbs}</TableCell>
+                  <TableCell align='right'>
+                    <Checkbox color='primary' />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 };
 
 export default AdminGameDashBoard;
