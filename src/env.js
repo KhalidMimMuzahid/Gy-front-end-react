d0ca17cee3d32dfaca6a3da752f61dfcabdb292d
KhalidMimMuzahid <khalidmimm@gmail.com>
2023-12-19 14:53:59 +0600

    updated

--- src/env.js
+++ src/env.js
@@ -1,8 +1,10 @@
 export const env = {
   SECRET_KEY:
     "71e1518c1cc9c5ffca9f68bb813b8fbf9d90008be91da2fc2f830d42b61633d887ec05cadbdbc9ec9ee168cf4b7b3846c7619f027db5c5b1339e2105826ba465",
-  BASE_URL: "http://localhost:8787",
-  // BASE_URL: "https://growmore.triplewsols.in",
+  // BASE_URL: "http://localhost:8787",
+  // // BASE_URL: "https://growmore.triplewsols.in",
+  BASE_URL: "https://grow-more-backend.vercel.app/",
+
   google_client_id:
     "350224658302-etk8h8jcju1qbrjri8nrkd0uamgs7a62.apps.googleusercontent.com",
 };
