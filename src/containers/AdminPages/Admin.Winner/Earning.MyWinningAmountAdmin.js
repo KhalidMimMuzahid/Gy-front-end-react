8e8de5167ae6d19d0138223c879a3c73c8cdcbd5
sarwar3w <sarwar@triplewsols.org>
2023-12-30 11:01:31 +0600

    table and color issue fixed

--- src/containers/AdminPages/Admin.Winner/Earning.MyWinningAmountAdmin.js
+++ src/containers/AdminPages/Admin.Winner/Earning.MyWinningAmountAdmin.js
@@ -3,18 +3,28 @@ import React from "react";
 import { IoMdTrophy } from "react-icons/io";
 import { useGetMyWinningHistoryAdminQuery } from "../../../Services/earningApi";
 import WinningAmountHistoryAdmin from "./Table/WinningAmountTableAdmin";
+import SectionCommonTable from "../../../components/SectionCommonTable";
+import { useState } from "react";
 
 const MyWinningAmountAdmin = () => {
+  const [filterData,setFilterData] = useState([])
   const { data: MyWinningHistoryAdmin } = useGetMyWinningHistoryAdminQuery();
   console.log("My Winning History :", MyWinningHistoryAdmin);
   return (
     <div className='records_table'>
-      <div className='table_header'>
-        <IoMdTrophy size={50} />
-        <p>All Winning Records</p>
-      </div>
       <div className='table-content'>
-        <WinningAmountHistoryAdmin data={MyWinningHistoryAdmin} />
+      <SectionCommonTable
+          wrapperClassName='roi_table'
+          setFilterData ={setFilterData}
+          cardStyle={{ backgroundColor: "#fff" }}
+          sectionTableTitle=' All Winning Records'
+          table={
+            <WinningAmountHistoryAdmin data={MyWinningHistoryAdmin || filterData}
+            // showDetails={showDetails}
+            
+            />
+          }
+        />
       </div>
     </div>
   );
--- src/containers/UserPages/Earnings/Earning.MyWinningAmount.js
+++ src/containers/UserPages/Earnings/Earning.MyWinningAmount.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import WinningAmountHistory from './Table/WinningAmountTable'
-import { IoMdTrophy } from 'react-icons/io';
-import { useGetMyWinningHistoryQuery } from '../../../Services/earningApi';
+import React from "react";
+import WinningAmountHistory from "./Table/WinningAmountTable";
+import { IoMdTrophy } from "react-icons/io";
+import { useGetMyWinningHistoryQuery } from "../../../Services/earningApi";
+import SectionCommonTable from "../../../components/SectionCommonTable";
+import RoiIncomeTable from "./Table/RoiIncomeTable";
 
 const MyWinningAmount = () => {
-    const { data: MyWinningHistory } = useGetMyWinningHistoryQuery();
-  console.log("My Winning History :",MyWinningHistory);
+  const { data: MyWinningHistory } = useGetMyWinningHistoryQuery();
+  console.log("My Winning History :", MyWinningHistory);
   return (
     <div className='records_table'>
-        <div className='table_header'>
-          <IoMdTrophy size={50} />
-          <p>My Winning Records</p>
-        </div>
-        <div className='table-content'>
-          <WinningAmountHistory data={MyWinningHistory} />
-        </div>
+      <div className='table-content'>
+        <SectionCommonTable
+          wrapperClassName='roi_table'
+          cardStyle={{ backgroundColor: "#fff" }}
+          sectionTableTitle=' My Winning Records'
+          table={
+            <WinningAmountHistory
+              data={MyWinningHistory}
+              // showDetails={showDetails}
+            />
+          }
+        />
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default MyWinningAmount
+export default MyWinningAmount;
